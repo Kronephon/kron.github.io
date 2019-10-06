28e2e86cea17305f91e8272053c536df0e1cf601
kronephon <kronephon@gmail.com>
2019-10-06 21:26:05 +0200

    added particle groundwork

--- assets/scripts/particleLib.js
+++ assets/scripts/particleLib.js
@@ -4,50 +4,90 @@
 
 // Draw whatever else over top of it on the canvas.
 
-window.addEventListener('resize', draw);
-var canvas = document.getElementById('backgroundCanvas');
-var X;
-var Y;
-var R;
+//init routine
+window.addEventListener('resize', updateScreenSize);
 
-var particle = {
-    width: 2, //expressed in pixels
-    x: 0,
-    y: 0
+var canvas = document.getElementById('backgroundCanvas');
+//if(!canvas){
+//    alert("no canvas");
+//}
+
+var windowsWidth;
+var windowsHeight;
+updateScreenSize();
+
+
+var particleSettings = {
+    width: 1 //expressed in pixels
 };
 
+var particleArray = [];
 
-function update() {
-    var win = window,
-        doc = document,
-        docElem = doc.documentElement,
-        body = doc.getElementsByTagName('body')[0],
-        x = win.innerWidth || docElem.clientWidth || body.clientWidth,
-        y = win.innerHeight || docElem.clientHeight || body.clientHeight;
-    canvas.width = win.innerWidth; // in pixels
-    canvas.height = win.innerHeight - 25; // in pixels
-
-    X = canvas.width / 2;
-    Y = canvas.height / 2;
-    R = Math.random() * 50;
+for (var i = 0; i < Math.floor(Math.random() * 20000); i++) { //todo: port number
+    var particle = {
+        width: particleSettings.width,
+        X: Math.random() * windowsWidth,
+        Y: Math.random() * windowsHeight,
+        Z: Math.random() * windowsHeight
+    }
+    particleArray[i] = particle;
+    // more statements
 }
 
 
-function draw() {
+requestAnimationFrame(mainLoop);
 
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        ctx.beginPath();
-        ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = '#FF0000';
-        ctx.stroke();
+function updateScreenSize(){
+    var win = window,
+    doc = document,
+    docElem = doc.documentElement,
+    body = doc.getElementsByTagName('body')[0],
+    x = win.innerWidth || docElem.clientWidth || body.clientWidth,
+    y = win.innerHeight || docElem.clientHeight || body.clientHeight;
+    windowsWidth = win.innerWidth; // in pixels
+    windowsHeight = win.innerHeight; // in pixels
+}
+
+function update() {
+    canvas.width = windowsWidth; // in pixels
+    canvas.height = windowsHeight - 25; // in pixels
+
+    for (var i = 0; i < particleArray.length; i++) {
+        particleArray[i].X = (particleArray[i].X + (Math.random() * 2 - 1)*0.1)  % windowsWidth;
+        particleArray[i].Y = (particleArray[i].Y + (Math.random() * 2 - 1)*0.1)  % windowsHeight;
+        particleArray[i].Z = (particleArray[i].Z + (Math.random() * 2 - 1)*0.1)  % windowsHeight;
     }
+}
+
+
+function getDoF(input) { //target is -1 to 1
+    var middle = windowsHeight/2;
+    var norm = (windowsHeight -input)/windowsHeight;
+    return Math.abs(norm);
+}
+
+function draw() {
+    var ctx = canvas.getContext('2d');
+
+    for (var i = 0; i < particleArray.length; i++) {
+        ctx.beginPath();
+        ctx.arc(particleArray[i].X, particleArray[i].Y, particleArray[i].width, 0, 2 * Math.PI, false);
+        ctx.lineWidth = getDoF(particleArray[i].Z)* 5;
+        ctx.strokeStyle = '#82718B';
+        ctx.stroke();
+        //Do something
+    }
+    //ctx.beginPath();
+    //ctx.arc(X, Y, R, 0, 2 * Math.PI, false);
+    //ctx.lineWidth = 3;
+    //ctx.strokeStyle = '#FF0000';
+    // ctx.stroke();
+
 }
 
 function mainLoop() {
 
-    if (canvas) {
+    if (canvas.getContext) {
         update();
         draw();
 
@@ -55,7 +95,3 @@ function mainLoop() {
     setTimeout(mainLoop, 1 / 60);
 
 }
-
-
-// Start things off
-//requestAnimationFrame(mainLoop);
