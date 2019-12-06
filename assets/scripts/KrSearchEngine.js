224dc12cbec602aa1d751b914eace0256882f006
Pedro <pedro.pimentel@1000shapes.com>
2019-12-06 15:31:43 +0100

    search engine: add tag based search. missing ordering and tag display

--- assets/scripts/KrSearchEngine.js
+++ assets/scripts/KrSearchEngine.js
@@ -1,12 +1,151 @@
-LIMIT_PER_PAGE = 100;
+---
 
-//receives a tag and goes through all the posts needed, if empty presents all pending limit
+---
+// script is very dependent on structure in feed.html as it generates html code for it
 
-function q(input) {
-    console.log(input);
-    container_block = document.getElementsByClassName('ArticleFeed')[0];
-    block_to_insert = document.createElement( 'div' );
-    block_to_insert.innerHTML = 'This demo DIV block was inserted into the page using JavaScript.' ;
-    console.log(container_block);
-    container_block.appendChild( block_to_insert ); 
+LIMIT_PER_PAGE = 5;
+
+var index = []
+
+var all = [];
+all.push("all");
+{% for post in site.posts %}
+all.push("{{post.url}}");
+{% endfor %}
+index.push(all);
+
+{% for tag in site.tags %}
+{% assign t = tag | first %}
+var {{t}} = [];
+{{t}}.push("{{t}}");
+{% for post in site.posts %}
+{% if post.tags contains t%}
+{{t}}.push("{{post.url}}");
+{% endif %}
+{% endfor %}
+index.push({{t}});
+{% endfor %}
+
+
+//receives a tag and goes through all the posts needed, if -1 presents all pending limit
+
+function query(input) {
+    result = [];
+    for (var i = 0; i < index.length; i++) {
+        if (index[i][0] == input) {
+            result = index[i];
+        }
+    }
+    console.log(result);
+    if (result.length > 1) {
+        insertFeed(result);
+    }
 }
+
+function insertFeed(result) {
+    
+    container_block = document.getElementsByClassName('ArticleFeed')[0];
+    if(container_block){
+        clearFeed(container_block);
+        for (var i = 1; i < result.length && i <= LIMIT_PER_PAGE; i++) {
+            //get data
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', result[i], true); //TODO requires performance boost
+            xhr.onreadystatechange = function() {
+                if (this.readyState !== 4) return;
+                if (this.status !== 200) return;
+                var ref = this.responseURL.substr(window.location.href.length -5);
+                parseAndBuild(this.responseText, container_block, ref);
+            };
+            xhr.send();
+        }
+    }
+}
+
+function parseAndBuild(htmlInput, destination_block, ref){ //TODO : requires sanitation
+    //title, ref, date, excerpt, tags
+
+    var dateRX = new RegExp(/.*?(<meta name=\"date\" content=\"([0-9]+ \w+ [0-9]+)\">).*?/g);
+    date = dateRX.exec(htmlInput)[2];
+
+    var titleRX = new RegExp(/(.*?<title>)(.*)(<\/title>.*?)/g);
+    title = titleRX.exec(htmlInput)[2];
+
+    var excerptRX = new RegExp(/.*?(<meta name=\"excerpt\" content=\"(.*)\">).*?/g);
+    excerpt = excerptRX.exec(htmlInput)[2];
+
+    var tagRX = new RegExp(/.*?(<meta name=\"tag_.+\" content=\"(.*)\">).*?/g);
+    var tags = [];
+    for (var i = 0; i < tagRX.length; i++) {
+        console.log(tagRX[i]);
+    }
+
+    insertArticle(destination_block, createBlock(title, ref, date, excerpt, tags));
+
+}
+
+function clearFeed(block) {
+    block.innerHTML = "";
+    console.log("-clear feed-"); //todo remove
+}
+
+function createBlock(title, ref, date, excerpt, tags) {
+
+    article = document.createElement('article');
+
+    divTitle = document.createElement('a');
+    divTitle.className = "Title";
+    divTitle.innerHTML = title;
+    divTitle.href = ref;
+    article.appendChild(divTitle);
+
+    divDate = document.createElement('div');
+    divDate.className = "Date";
+    divDate.innerHTML = date;
+    article.appendChild(divDate);
+
+    for (var i = 0; i < tags.length; i++) {
+
+        divTag = document.createElement('button');
+        divTag.className = "Tag";
+        divTag.type = "click";
+        divTag.value = tags[i];
+        divTag.onclick = function() {query(divTag.value);};
+        divTag.innerHTML = tags[i];
+        article.appendChild(divTag);
+
+    }
+    divExcerpt = document.createElement('div');
+    divExcerpt.className = "Excerpt";
+    divExcerpt.innerHTML = excerpt;
+    article.appendChild(divExcerpt);
+
+    return article;
+}
+
+function initFeed() {
+    container_block = document.getElementsByClassName('ArticleFeed')[0];
+    if (container_block) {
+        query("all");
+    }
+    console.log('-feed loaded-');
+}
+
+window.onload = initFeed();
+
+//title
+//date
+//tags
+//excert
+//get all
+
+/*
+<article class = "{% for tag in post.tags %}{{ tag }} {% endfor %}">
+<div class = Title><a href="{{ post.url }}">{{ post.title }}</a></div>
+<div class = "Date">{{ post.date | date_to_string }}</div>
+{% for tag in post.tags %}
+    <button class=Tag name="q" type="click" value="{{ tag }}" onClick="q('{{ tag }}')">{{ tag }}</button>
+{% endfor %}
+;
+<div class = "Excerpt">{{ post.excerpt }}</div>
+</article>*/
