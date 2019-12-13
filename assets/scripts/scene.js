8afd3b613e18dec3fcd359f04521ea13984c5931
Pedro <pedro.pimentel@1000shapes.com>
2019-12-13 16:05:40 +0100

    mobile test

--- assets/scripts/scene.js
+++ assets/scripts/scene.js
@@ -99,7 +99,7 @@ gltf => onLoad( gltf, )
 );
 
 function update() {
-  rotation = 0.05;
+  rotation += 0.005;
   CAMERA.position.x = Math.sin(rotation)* 500 + center.x;
   CAMERA.position.y = 300 + center.y;
   CAMERA.position.z = Math.cos(rotation)* 500  + center.y;
