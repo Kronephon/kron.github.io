ba1836c5f6041ebd6d988f02b46d737a9bf5ccd1
kronephon <kronephon@gmail.com>
2019-12-15 15:30:20 +0100

    partitioned css more appropriatly

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,8 +29,37 @@ function addLinks(input){
     }
 }
 
+function insertPost(post, htmldiv){
+    var article = document.createElement('div');
+    article.class = "article";
+
+    var title = document.createElement('a');
+    title.class = "articleTitle"
+    title.href = post.url;
+    title.innerHTML = post.title;
+
+    var date = document.createElement('div');
+    date.class = "articleDate"
+    date.innerHTML = post.date;
+
+    var text = document.createElement('div');
+    text.class = "articleText"
+    text.innerHTML = post.excerpt;
+
+    article.appendChild(title);
+    article.appendChild(date);
+    article.appendChild(text);
+
+
+    htmldiv.appendChild(article);
+}
+
 function parsePostsAndBuild(e, parent){
-    console.log(e);
+    if(e.detail){
+        insertPost(e.detail, parent);
+    }else{
+        console.log("parsePostsAndBuild: empty result");
+    }
 }
 
 addLinks('HeaderLinks');
