99ce52b4e745a0d3d8b9634770b048b018603d52
Pedro <pedro.pimentel@1000shapes.com>
2019-12-16 13:45:37 +0100

    fixed transformation bug

--- assets/js/dynamicBackground.js
+++ assets/js/dynamicBackground.js
@@ -18,10 +18,6 @@ class Point{
         this.x *= c;
         this.y *= c;
     }
-    applyMatrix(matrix){ //x'= kx
-        this.x = matrix[0][0]*this.x + matrix[1][0]*this.x;
-        this.y = matrix[0][1]*this.y + matrix[1][1]*this.y;
-    }
 }
 
 class TransMatrix{
@@ -33,22 +29,83 @@ class TransMatrix{
      */
     constructor(){
         this.c1 = 1;
-        this.c2 = c2;
-        this.c3 = c3;
-        this.c4 = c4;
-    }
-    applyMatrix(point){
-        point.x = this.c1*point.x + this.c2*point.x;
-        point.y = this.c3*point.y + this.c4*point.y;
-    }
-    translation(x,y){
+        this.c2 = 0;
+        this.c3 = 0;
 
+        this.c4 = 0;
+        this.c5 = 1;
+        this.c6 = 0;
+
+        this.c7 = 0;
+        this.c8 = 0;
+        this.c9 = 1;
+    }
+
+    applyMatrix(point){
+        var x = point.x, y = point.y;
+        point.x = this.c1*x + this.c2*y + this.c3;
+        point.y = this.c4*x + this.c5*y + this.c6;
+    }
+
+    copy(){
+        var matrix = new TransMatrix();
+        matrix.c1 = this.c1;
+        matrix.c2 = this.c2;
+        matrix.c3 = this.c3;
+
+        matrix.c4 = this.c4;
+        matrix.c5 = this.c5;
+        matrix.c6 = this.c6;
+
+        matrix.c7 = this.c7;
+        matrix.c8 = this.c8;
+        matrix.c9 = this.c9;
+        return matrix;
+    }
+
+    m(matrix){
+        var newMatrix = new TransMatrix();
+       
+        newMatrix.c1 = this.c1*matrix.c1 + this.c2*matrix.c4 + this.c3*matrix.c7;
+        newMatrix.c2 = this.c1*matrix.c2 + this.c2*matrix.c5 + this.c3*matrix.c8;
+        newMatrix.c3 = this.c1*matrix.c3 + this.c2*matrix.c6 + this.c3*matrix.c9;
+
+        newMatrix.c4 = this.c4*matrix.c1 + this.c5*matrix.c4 + this.c6*matrix.c7;
+        newMatrix.c5 = this.c4*matrix.c2 + this.c5*matrix.c5 + this.c6*matrix.c8;
+        newMatrix.c6 = this.c4*matrix.c3 + this.c5*matrix.c6 + this.c6*matrix.c9;
+        
+        newMatrix.c7 = this.c7*matrix.c1 + this.c8*matrix.c4 + this.c9*matrix.c7;
+        newMatrix.c8 = this.c7*matrix.c2 + this.c8*matrix.c5 + this.c9*matrix.c8;
+        newMatrix.c9 = this.c7*matrix.c3 + this.c8*matrix.c6 + this.c9*matrix.c9;
+
+        return newMatrix;
+    }
+
+    translation(x,y){
+        var newMatrix = new TransMatrix();
+
+        newMatrix.c3 = x;
+        newMatrix.c6 = y;
+
+        return this.m(newMatrix);
     }
     scale(s){
+        var newMatrix = new TransMatrix();
 
+        newMatrix.c1 = s;
+        newMatrix.c5 = s;
+
+        return this.m(newMatrix);
     }
     rotation(r){ //radians
+        var newMatrix = new TransMatrix();
 
+        newMatrix.c1 = Math.cos(r);
+        newMatrix.c2 = -Math.sin(r);
+        newMatrix.c4 = Math.sin(r);
+        newMatrix.c5 = Math.cos(r);
+
+        return this.m(newMatrix);
     }
 }
 
@@ -57,13 +114,12 @@ function drawHexagon(canvas, matrix, strokeColor, fillColor) {
         return;
     }
     
-    var points = [new Point(0.5, 1),
-        new Point(0.5, 1),
-        new Point(1, 0),
-        new Point(0.5, -1),
-        new Point(-0.5, -1),
-        new Point(-1, 0),
-        new Point(-0.5, 1)
+    var points = [new Point(0.5 ,  1),
+                  new Point(1   ,  0),
+                  new Point(0.5 , -1),
+                  new Point(-0.5, -1),
+                  new Point(-1  ,  0),
+                  new Point(-0.5,  1)
     ];
 
     points.forEach(point => {
@@ -92,6 +148,8 @@ function drawHexagonLit(canvas, p1, p2, p3 ,p4 ,p5, p6, strokeColor, fillColor){
     ctx.strokeStyle = strokeColor;
     ctx.fillColor = fillColor;
     ctx.stroke();
+    ctx.fill();
+    ctx.closePath();
 }
 
 
@@ -99,7 +157,19 @@ function dynamicBackgroundInit(canvas){
     if(!canvas){
         return;
     }
-    var worldMatrix = new TransMatrix(1,0,0,1);
-    console.log(worldMatrix);
-    drawHexagon(canvas, worldMatrix, "red", "green");
+    var worldMatrix = new TransMatrix();
+    
+    var rotate = 0;
+
+    function step(){
+        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+        rotate += 0.3;
+        var transform = worldMatrix.copy();
+        transform = worldMatrix.translation(200,200).scale(20).rotation(rotate); // TODO this is better reflected by a matrix inside an object
+        drawHexagon(canvas, transform, "red", "green");
+        window.requestAnimationFrame(step);
+    }
+
+    window.requestAnimationFrame(step);
 }
+
