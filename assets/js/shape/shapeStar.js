8014e3ba864b2df41e7b6c07e1b479b5736da008
kronephon <kronephon@gmail.com>
2020-01-05 16:16:37 +0100

    shape glsl : fragment shader prepared for diffuse work

--- assets/js/shape/shapeStar.js
+++ assets/js/shape/shapeStar.js
@@ -3,9 +3,6 @@ class MainStar_sp{
     this.clock = new THREE.Clock();
 
     this.uniforms = {
-      //cameraPosition: {type: 'vec3', value: new THREE.Vector3(0,0,0)},
-      colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
-      colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)},
       time:   {type: 'float', value: 2.0},
       meshPosition: {type: 'vec4', value: new THREE.Vector3(0,0,0)}
     }
@@ -20,26 +17,15 @@ class MainStar_sp{
 
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     scene_sp.add(this.mesh);
-
+    console.log(this.mesh);
   }
   update(){
     this.uniforms.time.value = this.clock.getElapsedTime();
-    this.uniforms.colorB.value.r = Math.random(); 
-    this.uniforms.colorB.value.g = Math.random(); 
-    this.uniforms.colorB.value.b = Math.random(); 
-    this.mesh.rotation.x += (Math.random() - 0.15) * 0.05;
-    this.mesh.rotation.y += (Math.random() - 0.25) * 0.05;
-    this.mesh.rotation.z += (Math.random() - 0.5) * 0.05;
+    this.mesh.rotation.x += (Math.random() - 0.15) * 0.01;
+    this.mesh.rotation.y += (Math.random() - 0.25) * 0.01;
+    this.mesh.rotation.z += (Math.random() - 0.5) * 0.01;
     this.uniforms.center = this.mesh.position;
     //this.uniforms.cameraPosition.value = camera_sp.position;
-  }
-
-
-  
-  fragmentShader() {
-    return `
-
-  `
   }
 }
   
--- assets/js/shape/starFragmentShader.glsl
+++ assets/js/shape/starFragmentShader.glsl
@@ -1,9 +1,19 @@
+uniform float time;
 varying float angle;
-  
-  void main() {
-    if(angle <= 0.50){
-      gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
-    }else{
-      gl_FragColor = vec4(angle, angle, angle, 1.0);
+varying vec2 UV;
+
+vec4 getDiffuseColor(vec2 coord, offset){
+    if(floor(mod(time * 10.0, 2.0)) == 0.0){
+        return vec4(1.0,1.0,1.0,0.0);
     }
- }
+    if(floor(mod(coord.x * 10.0, 2.0)) == 0.0){
+        return vec4(1.0,1.0,0.0,1.0);
+    }else{
+        return vec4(0.0,0.0,0.0,1.0);
+    }
+}
+
+void main() {
+    gl_FragColor = getDiffuseColor(UV, time);
+    
+}
