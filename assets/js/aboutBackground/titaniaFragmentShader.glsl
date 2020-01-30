7dfc1265891271a58e8ec7810e1cbfecb2e2defb
kronephon <kronephon@gmail.com>
2020-01-30 19:21:17 +0100

    aboutBackground: preliminary titania shader

--- assets/js/aboutBackground/titaniaFragmentShader.glsl
+++ assets/js/aboutBackground/titaniaFragmentShader.glsl
@@ -19,7 +19,31 @@ uniform vec3 meshPosition;
 
 varying vec3 worldPosition;
 
-void main() {
+void main(out vec4 fragColor, in vec2 fragCoord)
+{
+    vec2 uv = fragCoord.xy / iResolution.xy;
     
+    vec3 color = ColorFetch(uv);
+    
+    
+    color += GetBloom(uv) * 0.08;
+    
+    color *= 200.0;
+    
+
+    //Tonemapping and color grading
+    color = pow(color, vec3(1.5));
+    color = color / (1.0 + color);
+    color = pow(color, vec3(1.0 / 1.5));
+
+    
+    color = mix(color, color * color * (3.0 - 2.0 * color), vec3(1.0));
+    color = pow(color, vec3(1.3, 1.20, 1.0));    
+
+	color = saturate(color * 1.01);
+    
+    color = pow(color, vec3(0.7 / 2.2));
+
+    fragColor = vec4(color, 1.0);
 
 }
