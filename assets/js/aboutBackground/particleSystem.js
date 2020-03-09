9e96de380d97dc35c8252bbb978b7c36345d6f4d
Pedro <pedro.pimentel@1000shapes.com>
2020-03-09 17:49:58 +0100

    preliminary work in adding new utility index to the buffers

--- assets/js/aboutBackground/particleSystem.js
+++ assets/js/aboutBackground/particleSystem.js
@@ -4,55 +4,57 @@ class krParticleSystem{
         this.spawnChance = 0.8; //per frame new particles
         this.clock = new THREE.Clock();
 
-        var geometry = new THREE.IcosahedronBufferGeometry(1,0);
+        var geometry = new THREE.BufferGeometry();
         var material = new THREE.MeshNormalMaterial({
         });
-        this.pointMesh = new THREE.Points(geometry, material);
-        console.log(this.mesh);
 
         if(target == undefined){
             this.target = new THREE.IcosahedronBufferGeometry(1, 0);
         }else{
             this.target = target;
         }
+        geometry.setAttribute( 'position',  this.target.attributes.position); //TODO gl.DYNAMIC_DRAW add
+        geometry.setAttribute( 'enabled', new THREE.Float32BufferAttribute( this.target.attributes.position.count, 1 ) ); //enabled at 0
+        geometry.computeVertexNormals();
 
-        console.log(this.target);
-        geometry.setAttribute( 'position',  new krFloat32BufferAttribute(this.target.attributes.position.array, this.target.attributes.position.dimensions));
-        //
-		//geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( colors, 3 ) ); //or visible?
+        this.gateMesh = new THREE.Mesh(geometry, material);
 
-        console.log(this.pointMesh);
-        scene.add(this.pointMesh);
+        scene.add(this.gateMesh);
     }
+    
     pickNewParticle(){
-        if(this.target.attributes.position.count != 0){
-            var particleIndex = Math.random() * this.target.attributes.position.count;
-            //this.geometry.setAttribute( 'position',  this.geometry.attributes.position.push(new THREE.Vector3(0,0,0)));
-        }
+    
     }
     update(){
+        
         this.pickNewParticle();
-
     }
-}
 
-class krFloat32BufferAttribute extends THREE.Float32BufferAttribute{
-    constructor(array, dimensions){
-        super(array, dimensions);
+    getPointIndex(buffer){ // expects Float32BufferAttribute
+        var tmpBuffer = buffer.clone ();
+        var result = []; 
         
-        this.index = []; // each index in array will be here aswell. Also their unique content.
-        for(var i = 0; i < this.count; i++){
-            var point = [this.getX(i), this.getY(i), this.getZ(i)]; //this needs to be more flexible maybe
-            for(var j = 0; j < this.index.length; j++){
-
+        for(var i = 0; i < tmpBuffer.count; i++){
+            var point = [tmpBuffer.getX(i), tmpBuffer.getY(i), tmpBuffer.getZ(i)]; //this needs to be more flexible maybe
+            var pointIndex = [i]; 
+            tmpBuffer.setXYZ(i, 9999, 9999, 9999, 9999); 
+            for(var j = 0; j < tmpBuffer.count; j++){ // point ID could just be the index mix
+                var pointProbe = [tmpBuffer.getX(i), tmpBuffer.getY(i), tmpBuffer.getZ(i)];
+                if(pointProbe[0] == 9999 && pointProbe[1] == 9999, pointProbe[2] == 9999){
+                    continue;
+                }
+                if(pointProbe[0] == point[0] && pointProbe[1] == point[1], pointProbe[2] == point[2]){
+                    tmpBuffer.setXYZ(j, 9999, 9999, 9999, 9999); 
+                    pointIndex.push(j);
+                    continue;
+                }
             }
+            result.push(pointIndex);
         }
-
-        
+        console.log(result);
+    }
+    applyChangetoPoint(buffer, point, newValue){
 
     }
-    //updated index of common points
-    //automatic propagation of changes
-
 }
 
