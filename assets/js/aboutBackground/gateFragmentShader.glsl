8a1c759e8dcc83a6c90cc6b291f69bf4183b1a80
pedro <pedro.pimentel@1000shapes.com>
2020-05-15 19:30:14 +0200

    new shading work

--- assets/js/aboutBackground/gateFragmentShader.glsl
+++ assets/js/aboutBackground/gateFragmentShader.glsl
@@ -1,126 +1,3 @@
-/*
-vec4 mod289(vec4 x) {
-  return x - floor(x * (1.0 / 289.0)) * 289.0; }
-
-float mod289(float x) {
-  return x - floor(x * (1.0 / 289.0)) * 289.0; }
-
-vec4 permute(vec4 x) {
-     return mod289(((x*34.0)+1.0)*x);
-}
-
-float permute(float x) {
-     return mod289(((x*34.0)+1.0)*x);
-}
-
-vec4 taylorInvSqrt(vec4 r)
-{
-  return 1.79284291400159 - 0.85373472095314 * r;
-}
-
-float taylorInvSqrt(float r)
-{
-  return 1.79284291400159 - 0.85373472095314 * r;
-}
-
-vec4 grad4(float j, vec4 ip)
-  {
-  const vec4 ones = vec4(1.0, 1.0, 1.0, -1.0);
-  vec4 p,s;
-
-  point.xyz = floor( fract (vec3(j) * ipoint.xyz) * 7.0) * ipoint.z - 1.0;
-  point.w = 1.5 - dot(abs(point.xyz), ones.xyz);
-  s = vec4(lessThan(p, vec4(0.0)));
-  point.xyz = point.xyz + (s.xyz*2.0 - 1.0) * s.www; 
-
-  return p;
-  }
-						
-// (sqrt(5) - 1)/4 = F4, used once below
-#define F4 0.309016994374947451
-
-float snoise(vec4 v)
-  {
-  const vec4  C = vec4( 0.138196601125011,  // (5 - sqrt(5))/20  G4
-                        0.276393202250021,  // 2 * G4
-                        0.414589803375032,  // 3 * G4
-                       -0.447213595499958); // -1 + 4 * G4
-
-// First corner
-  vec4 i  = floor(v + dot(v, vec4(F4)) );
-  vec4 x0 = v -   i + dot(i, C.xxxx);
-
-// Other corners
-
-// Rank sorting originally contributed by Bill Licea-Kane, AMD (formerly ATI)
-  vec4 i0;
-  vec3 isX = step( x0.yzw, x0.xxx );
-  vec3 isYZ = step( x0.zww, x0.yyz );
-//  i0.x = dot( isX, vec3( 1.0 ) );
-  i0.x = isX.x + isX.y + isX.z;
-  i0.yzw = 1.0 - isX;
-//  i0.y += dot( isYZ.xy, vec2( 1.0 ) );
-  i0.y += isYZ.x + isYZ.y;
-  i0.zw += 1.0 - isYZ.xy;
-  i0.z += isYZ.z;
-  i0.w += 1.0 - isYZ.z;
-
-  // i0 now contains the unique values 0,1,2,3 in each channel
-  vec4 i3 = clamp( i0, 0.0, 1.0 );
-  vec4 i2 = clamp( i0-1.0, 0.0, 1.0 );
-  vec4 i1 = clamp( i0-2.0, 0.0, 1.0 );
-
-  //  x0 = x0 - 0.0 + 0.0 * C.xxxx
-  //  x1 = x0 - i1  + 1.0 * C.xxxx
-  //  x2 = x0 - i2  + 2.0 * C.xxxx
-  //  x3 = x0 - i3  + 3.0 * C.xxxx
-  //  x4 = x0 - 1.0 + 4.0 * C.xxxx
-  vec4 x1 = x0 - i1 + C.xxxx;
-  vec4 x2 = x0 - i2 + C.yyyy;
-  vec4 x3 = x0 - i3 + C.zzzz;
-  vec4 x4 = x0 + C.wwww;
-
-// Permutations
-  i = mod289(i); 
-  float j0 = permute( permute( permute( permute(i.w) + i.z) + i.y) + i.x);
-  vec4 j1 = permute( permute( permute( permute (
-             i.w + vec4(i1.w, i2.w, i3.w, 1.0 ))
-           + i.z + vec4(i1.z, i2.z, i3.z, 1.0 ))
-           + i.y + vec4(i1.y, i2.y, i3.y, 1.0 ))
-           + i.x + vec4(i1.x, i2.x, i3.x, 1.0 ));
-
-// Gradients: 7x7x6 points over a cube, mapped onto a 4-cross polytope
-// 7*7*6 = 294, which is close to the ring size 17*17 = 289.
-  vec4 ip = vec4(1.0/294.0, 1.0/49.0, 1.0/7.0, 0.0) ;
-
-  vec4 p0 = grad4(j0,   ip);
-  vec4 p1 = grad4(j1.x, ip);
-  vec4 p2 = grad4(j1.y, ip);
-  vec4 p3 = grad4(j1.z, ip);
-  vec4 p4 = grad4(j1.w, ip);
-
-// Normalise gradients
-  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
-  p0 *= norm.x;
-  p1 *= norm.y;
-  p2 *= norm.z;
-  p3 *= norm.w;
-  p4 *= taylorInvSqrt(dot(p4,p4));
-
-// Mix contributions from the five corners
-  vec3 m0 = max(0.6 - vec3(dot(x0,x0), dot(x1,x1), dot(x2,x2)), 0.0);
-  vec2 m1 = max(0.6 - vec2(dot(x3,x3), dot(x4,x4)            ), 0.0);
-  m0 = m0 * m0;
-  m1 = m1 * m1;
-  return 49.0 * ( dot(m0*m0, vec3( dot( p0, x0 ), dot( p1, x1 ), dot( p2, x2 )))
-               + dot(m1*m1, vec2( dot( p3, x3 ), dot( p4, x4 ) ) ) ) ;
-
-}
-
-float densityAt(vec3 point){
-    return snoise(vec4(point + 5.0, clock * 0.05));
-} */
-
 uniform vec3 colorAmbient;            
 uniform vec3 colorDiffuse;
 uniform vec3 colorSpecular;
@@ -139,6 +16,12 @@ struct Ray{
     vec3 pos;
     vec3 dir;
 };
+
+float random (in vec2 _st) {
+    return fract(sin(dot(_st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
 
 float sdSphere( vec3 p, float s )
 {
@@ -221,71 +104,59 @@ float opCheapBend( in sdf3d primitive, in vec3 p )
     vec3  q = vec3(m*p.xy,p.z);
     return primitive(q);
 }*/
-const mat2 m = mat2( 0.80,  0.60, -0.60,  0.80 );
+// Based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise (in vec2 _st) {
+    vec2 i = floor(_st);
+    vec2 f = fract(_st);
 
-float hash( vec2 p )
-{
-	float h = dot(p,vec2(127.1,311.7));
-    return -1.0 + 2.0*fract(sin(h)*43758.5453123);
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    vec2 u = f * f * (3.0 - 2.0 * f);
+
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
 }
 
-float noise( in vec2 p )
-{
-    vec2 i = floor( p );
-    vec2 f = fract( p );
-	
-	vec2 u = f*f*(3.0-2.0*f);
+#define NUM_OCTAVES 10
 
-    return mix( mix( hash( i + vec2(0.0,0.0) ), 
-                     hash( i + vec2(1.0,0.0) ), u.x),
-                mix( hash( i + vec2(0.0,1.0) ), 
-                     hash( i + vec2(1.0,1.0) ), u.x), u.y);
+float fbm ( in vec2 _st) {
+    float v = 0.0;
+    float a = 0.5;
+    vec2 shift = vec2(100.0);
+    // Rotate to reduce axial bias
+    mat2 rot = mat2(cos(0.5), sin(0.5),
+                    -sin(0.5), cos(0.50));
+    for (int i = 0; i < NUM_OCTAVES; ++i) {
+        v += a * noise(_st);
+        _st = rot * _st * 2.0 + shift;
+        a *= 0.5;
+    }
+    return v;
 }
-
-float fbm( vec2 p )
-{
-    float f = 0.0;
-    f += 0.5000*noise( p ); p = m*p*2.02;
-    f += 0.2500*noise( p ); p = m*p*2.03;
-    f += 0.1250*noise( p ); p = m*p*2.01;
-    f += 0.0625*noise( p );
-    return f/0.9375;
-}
-
-vec2 fbm2( in vec2 p )
-{
-    return vec2( fbm(p.xy), fbm(p.yx) );
-}
-vec3 map( vec2 p )
-{   
-    p *= 0.7;
-
-    float f =  fbm2(-0.05*clock+2.0*(p + fbm2(4.0*p))).x;
-
-    float bl = smoothstep( -0.8, 0.8, f );
-
-    float ti = smoothstep( -1.0, 1.0, fbm(p) );
-
-    return mix( mix( vec3(0.50,0.00,0.00), 
-                     vec3(1.00,0.75,0.35), ti ), 
-                     vec3(0.00,0.00,0.02), bl );
-}
-
 
 #define maxSphere sdSphere(point, 1.0)
 
 float sceneSDF(vec3 point){
-    float shapeSphere = sdSphere(point, 0.9);
+    float shapeSphere = sdSphere(point, 0.8);
+    float shapeCenter = sdSphere(point, 0.1);
     float cone = sdRoundCone(point, 0.02, 0.7, 0.9);
 
     float halfMoon = smoothSubtractionSDF(cone, shapeSphere, 0.1);
-    //float disp = mod(clock*0.1,3.5)*mod(clock*0.1,-3.5);
+    float halfMoonC = smoothSubtractionSDF(shapeCenter, halfMoon, 0.1);
+    
+    float dispT = 2.3  * sin(clock*0.1);
+    
+    float final = fbm(point.xy) * dispT;
     //float displacement = sin(disp*point.x)*sin(disp*point.y)*sin(disp*point.z);
-    float displacement = length(map(vec2(point.xy)));
+    //float displacement = noise(vec2(disp, disp));
     //float removeCenter = smoothSubtractionSDF(sdSphere(point, 0.01), shapeOrigin + displacement, 1.0);
-    //return smoothIntersectionSDF(maxSphere, shapeOrigin + displacement, 1.0);
-
-    return halfMoon + displacement;
+    return smoothIntersectionSDF(maxSphere, halfMoonC  - 0.2 *final, 0.01);
 }
 
 vec3 estimateNormal(vec3 p) {
@@ -301,13 +172,13 @@ vec3 shade(vec3 point, vec3 direction){ // using phong for now
     float specularity = 1.0;
     float diffuse = 1.0;
     float ambient = 1.0;
-    float shinniness = 100.0;
+    float shinniness = 10.0;
 
     vec3 ambientColor = colorAmbient;
     vec3 diffuseColor = colorDiffuse;
     vec3 specularColor = colorSpecular;
     
-    Light mainLight = Light(vec3(0.0,0.0,1.5), 1.0, vec3(1.0,1.0,1.0));
+    Light mainLight = Light(vec3(0.0,0.0,0.0), 1.0, vec3(1.0,1.0,1.0));
     vec3 lightVector = normalize(mainLight.pos - point);
     vec3 normal  = normalize(estimateNormal(point));
     vec3 reflected = normalize(2.0 * dot(lightVector, normal) * normal - lightVector);
@@ -340,9 +211,14 @@ vec4 rayMarch(Ray ray){
         }
         float dist = sceneSDF(pos);
 
-        //if(length(pos) <= 0.1){
-        //    extra += vec4(0.14,0.1,0.04,0.15);
-        //}
+        if(length(pos) <= 0.1){
+            extra += vec4(0.14,0.1,0.04,0.15);
+        }
+
+        
+        if(dist <= 0.1){
+            extra += vec4(0.04,0.02,0.02,0.05);
+        }
         
         if(dist <= 0.0){
             result = vec4(shade(pos, dir),1.0);
