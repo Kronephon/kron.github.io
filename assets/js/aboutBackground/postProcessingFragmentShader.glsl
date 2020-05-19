4ed7c596abe376ff10ee83e8d0537b45ec1c30ae
Iberius Kronephon <krone@gitlab.com>
2020-05-19 23:07:35 +0200

    post processing work

--- assets/js/aboutBackground/postProcessingFragmentShader.glsl
+++ assets/js/aboutBackground/postProcessingFragmentShader.glsl
@@ -16,10 +16,10 @@ float random (in vec2 _st) {
 void main() {
     vec4 previousPassColor = texture2D(tDiffuse, vUv);
     if(clicked){
-            if(previousPassColor.x <= 0.8){
-                previousPassColor = texture2D(tDiffuse, vec2(vUv.x + 0.02 * (random(vec2(clock, vUv.y)) - 0.5)
-                                                        ,vUv.y + 0.02 * (random(vec2(clock, vUv.x)) - 0.5)));
-                previousPassColor.x = 0.0;                                                        
+            if(previousPassColor.x<= 1.0){
+                previousPassColor = texture2D(tDiffuse, vec2(vUv.x + 0.07 * (random(vec2(clock, vUv.y)) - 0.5)
+                                                        ,vUv.y + 0.001 * (random(vec2(clock, vUv.x)) - 0.5)));
+                previousPassColor.x = previousPassColor.y = previousPassColor.z;                                         
             }
     }
     
